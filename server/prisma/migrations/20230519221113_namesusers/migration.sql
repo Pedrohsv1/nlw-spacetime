ad4a37d27b0a3e6df306ca0d177bfc6ef32430fc
Pedro Varela <pedrao.varela@gmail.com>
2023-05-19 20:19:46 -0300

    first commit

---
+++ server/prisma/migrations/20230519221113_namesusers/migration.sql
@@ -0,0 +1,24 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `avatar` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `nome` on the `User` table. All the data in the column will be lost.
+  - Added the required column `avatarURL` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `name` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "githubId" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "login" TEXT NOT NULL,
+    "avatarURL" TEXT NOT NULL
+);
+INSERT INTO "new_User" ("githubId", "id", "login") SELECT "githubId", "id", "login" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_githubId_key" ON "User"("githubId");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
